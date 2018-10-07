15735c932d0d3f05067301e54f1e76a64395cb7f
lchsk <mjlechowski@gmail.com>
2018-10-07 20:00:53 +0100

    Refactor the main file a bit

--- src/derid.cpp
+++ src/derid.cpp
@@ -5,27 +5,14 @@
 #include "pos.hpp"
 
 int main(int argc, char *argv[]) {
-  derid::curses c;
+  derid::curses curses;
 
   // 4 empty lines under the list
-  derid::widget::list l(derid::pos(1, 0), c.size.get_row() - 5);
+  derid::widget::list list(derid::pos(1, 0), curses.size.get_row() - 5);
   derid::widget::label label(derid::pos(0, 0), "");
 
-  std::vector<std::string> t;
+  curses.l = &list;
+  curses.label = &label;
 
-  for (int i = 0; i < 20; i++) {
-    std::string s;
-    s += std::to_string(i + 1) + ") ";
-
-    for (int j = 0; j < rand() % 10 + 2; j++) {
-
-      s += "a";
-    }
-    t.push_back(s);
-  }
-
-  c.l = &l;
-  c.label = &label;
-
-  c.run();
+  curses.run();
 }
